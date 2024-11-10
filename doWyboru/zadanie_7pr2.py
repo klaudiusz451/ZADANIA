b7f527cf4221b145e61b7142618233b8b4f9114b
klaudiusz451 <kkapala@student.agh.edu.pl>
2024-11-10 11:06:13 +0100

    Update zadanie_7pr2.py

--- doWyboru/zadanie_7pr2.py
+++ doWyboru/zadanie_7pr2.py
@@ -1,22 +1,16 @@
-def fact(N):
-    x=1
-    for i in range(1,N+1):
-        x=i*x
-    return x
-
-
-list=[]
 try:
     m=int(input("Podaj koniec zakresu : "))
     if m<1 or m>10000:
         print("BŁĄD ZAKRESU")
     else:
+        y=1
+        list=[]
         for i in range(1,m+1):
-            k=fact(i)
-            print(f"{i}! = {k}")
-            list.append(k)
-        print("RÓŻNICE")
-        for i in range(1,m):
-            print(f"{list[i]}-{list[i-1]}={list[i]-list[i-1]}")
+           x=i*y
+           print(f"{i}! = {x}")
+           list.append(x)
+           y=x
+           if i>1:
+                print(f"{list[i-1]}-{list[i-2]}={list[i-1]-list[i-2]}")
 except ValueError:
-    print("BŁĄD")        
+    print("BŁĄD")
